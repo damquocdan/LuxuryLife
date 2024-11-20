ddd2301199eb5269b43722c86449286dcbcb1300
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-11-20 09:08:49 +0700

    20/11

--- LuxuryLife/LuxuryLife/Areas/CustomerUser/Controllers/DashboardController.cs
+++ LuxuryLife/LuxuryLife/Areas/CustomerUser/Controllers/DashboardController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LuxuryLife.Controllers;
+using LuxuryLife.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LuxuryLife.Areas.CustomerUser.Controllers
 {
     public class DashboardController : BaseController
     {
-        public IActionResult Index(int? id)
+        private readonly ILogger<DashboardController> _logger;
+        private readonly LuxuryLifeContext _context; // Thêm DbContext
+
+        public DashboardController(ILogger<DashboardController> logger, LuxuryLifeContext context)
         {
-            return View();
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tours = _context.Tours.Include(t => t.Provider).ToList();
+            return View(tours);
         }
     }
 }
