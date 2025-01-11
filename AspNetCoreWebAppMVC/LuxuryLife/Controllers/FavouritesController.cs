1dca72aaff2bb1362e6742e652fd017d133426d5
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2025-01-11 22:29:52 +0700

    Cap nhat customer view

--- AspNetCoreWebAppMVC/LuxuryLife/Controllers/FavouritesController.cs
+++ AspNetCoreWebAppMVC/LuxuryLife/Controllers/FavouritesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using LuxuryLife.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace LuxuryLife.Controllers
 {
@@ -18,12 +19,29 @@ namespace LuxuryLife.Controllers
             _context = context;
         }
 
-        // GET: Favourites
+        // GET: CustomerUser/Favorites
         public async Task<IActionResult> Index()
         {
-            var tourbookingContext = _context.Favourites.Include(f => f.Customer).Include(f => f.Tour);
-            return View(await tourbookingContext.ToListAsync());
+            // Lấy CustomerId từ session
+            var customerId = HttpContext.Session.GetInt32("CustomerId");
+
+            // Kiểm tra nếu CustomerId không tồn tại (người dùng chưa đăng nhập)
+            if (customerId == null)
+            {
+                // Chuyển hướng đến trang đăng nhập
+                return RedirectToAction("Index", "Login");
+            }
+
+            // Lọc danh sách yêu thích theo CustomerId
+            var luxuryLifeContext = _context.Favourites
+                .Include(f => f.Customer)
+                .Include(f => f.Tour)
+                .Where(f => f.CustomerId == customerId); // Lọc theo CustomerId
+
+            // Trả danh sách yêu thích của người dùng đến view
+            return View(await luxuryLifeContext.ToListAsync());
         }
+
         // ADD
         [HttpGet]
         public async Task<IActionResult> Add(int id)
@@ -35,7 +53,7 @@ namespace LuxuryLife.Controllers
             if (customerId == null)
             {
                 TempData["Error"] = "Bạn cần đăng nhập để sử dụng chức năng này.";
-                return RedirectToAction("Index", "Login", new { area = "CustomerUser" });
+                return RedirectToAction("Index", "Login");
 
             }
 
@@ -62,8 +80,7 @@ namespace LuxuryLife.Controllers
             TempData["Success"] = "Tour đã được thêm vào danh sách yêu thích!";
             return RedirectToAction("Index", "Home");
         }
-
-        // GET: Favourites/Details/5
+        // GET: CustomerUser/Favorites/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -80,36 +97,39 @@ namespace LuxuryLife.Controllers
                 return NotFound();
             }
 
-            return View(favourite);
+            return PartialView("_FavoriteDetail", favourite);
         }
 
-        // GET: Favourites/Create
+
+
+        // GET: CustomerUser/Favorites/Create
         public IActionResult Create()
         {
             ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId");
+            ViewData["HomestayId"] = new SelectList(_context.Homestays, "HomestayId", "HomestayId");
             ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId");
             return View();
         }
 
-        // POST: Favourites/Create
+        // POST: CustomerUser/Favorites/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FavoriteId,CustomerId,TourId,Sumprice")] Favourite favourite)
+        public async Task<IActionResult> Create([Bind("FavoriteId,CustomerId,HomestayId,TourId,CreateDate")] Favourite favorite)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(favourite);
+                _context.Add(favorite);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", favourite.CustomerId);
-            ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId", favourite.TourId);
-            return View(favourite);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", favorite.CustomerId);
+            ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId", favorite.TourId);
+            return View(favorite);
         }
 
-        // GET: Favourites/Edit/5
+        // GET: CustomerUser/Favorites/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -117,24 +137,24 @@ namespace LuxuryLife.Controllers
                 return NotFound();
             }
 
-            var favourite = await _context.Favourites.FindAsync(id);
-            if (favourite == null)
+            var favorite = await _context.Favourites.FindAsync(id);
+            if (favorite == null)
             {
                 return NotFound();
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", favourite.CustomerId);
-            ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId", favourite.TourId);
-            return View(favourite);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", favorite.CustomerId);
+            ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId", favorite.TourId);
+            return View(favorite);
         }
 
-        // POST: Favourites/Edit/5
+        // POST: CustomerUser/Favorites/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("FavoriteId,CustomerId,TourId,Sumprice")] Favourite favourite)
+        public async Task<IActionResult> Edit(int id, [Bind("FavoriteId,CustomerId,HomestayId,TourId,CreateDate")] Favourite favorite)
         {
-            if (id != favourite.FavoriteId)
+            if (id != favorite.FavoriteId)
             {
                 return NotFound();
             }
@@ -143,12 +163,12 @@ namespace LuxuryLife.Controllers
             {
                 try
                 {
-                    _context.Update(favourite);
+                    _context.Update(favorite);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!FavouriteExists(favourite.FavoriteId))
+                    if (!FavoriteExists(favorite.FavoriteId))
                     {
                         return NotFound();
                     }
@@ -159,12 +179,12 @@ namespace LuxuryLife.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", favourite.CustomerId);
-            ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId", favourite.TourId);
-            return View(favourite);
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "CustomerId", "CustomerId", favorite.CustomerId);
+            ViewData["TourId"] = new SelectList(_context.Tours, "TourId", "TourId", favorite.TourId);
+            return View(favorite);
         }
 
-        // GET: Favourites/Delete/5
+        // GET: CustomerUser/Favorites/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -172,36 +192,57 @@ namespace LuxuryLife.Controllers
                 return NotFound();
             }
 
-            var favourite = await _context.Favourites
+            var favorite = await _context.Favourites
                 .Include(f => f.Customer)
                 .Include(f => f.Tour)
                 .FirstOrDefaultAsync(m => m.FavoriteId == id);
-            if (favourite == null)
+            if (favorite == null)
             {
                 return NotFound();
             }
 
-            return View(favourite);
+            return View(favorite);
         }
 
-        // POST: Favourites/Delete/5
+        // POST: CustomerUser/Favorites/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var favourite = await _context.Favourites.FindAsync(id);
-            if (favourite != null)
+            var favorite = await _context.Favourites.FindAsync(id);
+            if (favorite != null)
             {
-                _context.Favourites.Remove(favourite);
+                _context.Favourites.Remove(favorite);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool FavouriteExists(int id)
+        private bool FavoriteExists(int id)
         {
             return _context.Favourites.Any(e => e.FavoriteId == id);
         }
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            // Find the favourite item by its ID
+            var favourite = _context.Favourites.FirstOrDefault(f => f.FavoriteId == id);
+
+            if (favourite == null)
+            {
+                return NotFound();
+            }
+
+            // Remove the item from the database
+            _context.Favourites.Remove(favourite);
+            _context.SaveChanges();
+
+            // Return a partial view or a success response
+            return Json(new { success = true, message = "Item removed successfully!" });
+        }
+        [Authorize]
+        public IActionResult PaymentCallBack() { return View(); }
+
     }
 }
---
+++ AspNetCoreWebAppMVC/LuxuryLife/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using LuxuryLife.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuxuryLife.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly TourBookingContext _context;
+
+        public LoginController(TourBookingContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginUser model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không hợp lệ.");
+                return View(model);
+            }
+
+            var dataLogin = _context.Customers
+                .Where(x => x.Email == model.Email && x.Password == model.Password)
+                .FirstOrDefault();
+
+            if (dataLogin != null)
+            {
+                HttpContext.Session.SetString("CustomerLogin", model.Email);
+                HttpContext.Session.SetInt32("CustomerId", dataLogin.CustomerId);
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Thông tin đăng nhập không chính xác.");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("CustomerLogin");
+            HttpContext.Session.Remove("CustomerId");
+
+            return RedirectToAction("Index");
+        }
+    }
+}
